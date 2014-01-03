e46b1a093e65e61b935dafaf9e897e864cf40a2c
Larson Hogstrom <hogstrom@broadinstitute.org>
2014-01-02 23:03:19 -0500

    updated wolfBot to now use Mike's CL scraper

--- wolfTwitter/wolfBot.py
+++ wolfTwitter/wolfBot.py
@@ -7,11 +7,11 @@ content containing a specific phrase
 '''
 
 import twitter, os, time, sys
-from random import choice
-from config import Config
 import mechanize
 import cookielib
+import pandas as pd
 from BeautifulSoup import BeautifulSoup
+import craigslist_scraper.cl_scraper as csc
 
 class howl_tweet(object):
     '''
@@ -46,60 +46,30 @@ class howl_tweet(object):
             access_token_key=keyDict['access_token_key'],
             access_token_secret=keyDict['access_token_secret'])
         self.api = api
-    
-    def get_craigslist_links(self):
+
+    def scrape_craigslist_links(self):
         '''
-        search items for sale on craigslist containing
-        a specific phrase 
+        Use the craigslist_scraper module to search CL and get urls 
         '''
-        # Set paramaters for Browser object 
-        br = mechanize.Browser()
-        # Cookie Jar
-        cj = cookielib.LWPCookieJar()
-        br.set_cookiejar(cj)
-        # Browser options
-        br.set_handle_equiv(True)
-        br.set_handle_gzip(True)
-        br.set_handle_redirect(True)
-        br.set_handle_referer(True)
-        br.set_handle_robots(False)
-        # Follows refresh 0 but not hangs on refresh > 0
-        br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
-        #open craigslist 
-        url = 'http://dallas.craigslist.org/'
-        br.open(url)
-        br.select_form(nr=0)
-        br.form['query'] = '"dog not included"' #see form.controls
-        br.submit()
-        html = br.response().read()
-        soup = BeautifulSoup(html)
-        titleTag = soup.html.head.title
-        #make a list of the CL findings
-        body1 = soup.html.body.contents[3]
-        body2 = body1.contents[5]
-        links2 = body2.findAll('a')
-        linkList = []
-        for link in links2:
-            if len(link.attrs) > 1:
-                tup1 = link.attrs[1]
-                if (tup1[0] == 'class') and (tup1[1] == 'i'):
-                    tup2 = link.attrs[0]
-                    resLink = tup2[1]
-                    if resLink[:4] == 'http':
-                        linkList.append(resLink)
-                    else:
-                        linkList.append(url + resLink)
-        self.linkList = linkList
+        cities = ['boston','nyc','sf','philadelphia','portland','fortcollins']
+        listingFrame = pd.DataFrame()
+        for city in cities:
+            cs = csc.cl_scraper(location=city,query='"dog not included"')
+            cs.run_cl_query()
+            cs.extract_listings(title_strict=False)
+            listingFrame = pd.concat([listingFrame,cs.craigslist_results],axis=0)
+        self.listingFrame = listingFrame
+
 
     def post_craigslist_links(self,n_posts=5):
         '''
         post links from craigslist search 
         '''
-        for ilink,link in enumerate(self.linkList[:n_posts]):
-            post = 'dog not included - post ' + str(ilink+1) + ' ' + link
+        for link in self.listingFrame['url'][:n_posts]:
+            post = 'dog not included ' + link
             print post 
             status = self.api.PostUpdate(post)
-            time.sleep(10)
+            time.sleep(2)
     
     def retweet_search_results(self,search_phrase='dog not included',n_posts=5):
         '''
--- wolfTwitter/wolfBot_script.py
+++ wolfTwitter/wolfBot_script.py
@@ -1,11 +1,11 @@
 'script to utilize the wolfBot twitter class'
 
-import wolfBot
+import wolfTwitter.wolfBot as wolfBot
 
 #reload(wolfBot)
 credFile = '/Users/hogstrom/Documents/code/configs/bot2.cfg'
 wb = wolfBot.howl_tweet(credFile)
-wb.get_craigslist_links()
+# wb.get_craigslist_links()
+wb.scrape_craigslist_links()
+wb.retweet_search_results(search_phrase='dogs not included',n_posts=5)
 wb.post_craigslist_links(n_posts=2)
-wb.retweet_search_results(search_phrase='dogs not included',n_posts=2)
-
